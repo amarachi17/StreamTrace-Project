2e4e3d977a8182ee54922623f073f8f8d6bfa823
Confidence Nkeonye <confidencenkeonye@gmail.com>
2023-08-11 22:15:25 +0100

    New changes

---
+++ index.js
@@ -0,0 +1,18 @@
+var scrollUpBtn = document.getElementById("scrollUpBtn");
+
+
+window.addEventListener("scroll", () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollUpBtn.style.display = "block";
+  } else {
+    scrollUpBtn.style.display = "none";
+  }
+});
+
+
+scrollUpBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
